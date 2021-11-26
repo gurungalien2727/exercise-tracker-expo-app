8a4a0ccd94f7712abbdef1fbdfb396ad7317ca9c
Alien Gurung <gurungalien2727@gmail.com>
2021-11-26 06:11:54 -0600

    called local api from expo app

--- components/ExerciseList.js
+++ components/ExerciseList.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import {Text} from 'react-native';
+import Constants from "expo-constants";
+
+const { manifest } = Constants;
 
 
 const ExerciseList = () =>{
-return (
+    const uri = `http://${manifest.debuggerHost.split(':').shift()}:4000/users`;
+    console.log('Inside exercise list component')
+    const [users, setUsers] = useState([]);
+    useEffect(()=>{
+        console.log('Inside useEffect hook')
+     fetch(uri)
+     .then(res => res.json())
+     .then(data => console.log(data))
+    },[])
+ return (
     <>
     <Text>ExerciseList</Text>
     </>
