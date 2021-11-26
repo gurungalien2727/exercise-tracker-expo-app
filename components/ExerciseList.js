e72dde3da2730069786fe5d433a0f991d516b154
Alien Gurung <gurungalien2727@gmail.com>
2021-11-26 04:14:03 -0600

    first commit

---
+++ components/ExerciseList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+
+const ExerciseList = () =>{
+return (
+    <>
+    <Text>ExerciseList</Text>
+    </>
+)
+}
+export default ExerciseList;
